04935bc79e7d1b312618adf92e2404701e7c5437
MGibson7 <MGibsonASC@gmail.com>
2022-04-20 12:01:45 -0400

    early javascript renderings

--- main.js
+++ main.js
@@ -5,3 +5,20 @@ addItem.addEventListener('click', event =>{
     document.getElementById("overlay").style.display = "grid";
 });
 
+//store user input
+let assets = []
+let liabilities = []
+
+//function that takes user input and puts it into array
+const form = document.getElementById("itemAdding");
+form.addEventListener('submit', event =>{
+        let form = document.getElementById("itemAdding");
+        let item = form.elements['type'];
+        if(item.value == "asset"){
+            console.log("asset");
+        }
+
+
+})
+
+
