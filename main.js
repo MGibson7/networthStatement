0dc90fdedeecaf440ab9bd69cdcb1e1b489d65cc
MGibson7 <MGibsonASC@gmail.com>
2022-04-20 10:30:10 -0400

    added javascript functionality

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ addItem.addEventListener('click', event =>{
     console.log("CLICKED"); 
     document.getElementById("overlay").style.display = "block";
 });
+
+const submit = document.getElementById("submit");
+submit.addEventListener('click', event =>{
+    console.log("CLICKED"); 
+    document.getElementById("overlay").style.display = "none";
+});
