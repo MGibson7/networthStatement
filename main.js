41ab784d9f7585d38b7577cbacf60faf7a37681b
MGibson7 <MGibsonASC@gmail.com>
2022-04-21 09:46:41 -0400

    added age range form to not overcrowd website display

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ addItem.addEventListener('click', event =>{
     document.getElementById("overlay").style.display = "grid";
 });
 
+const updateAge = document.getElementById("currentAge");
+
+updateAge.addEventListener('click', event =>{
+    console.log("CLICKED"); 
+    document.getElementById("overlay2").style.display = "grid";
+});
+
+let agerange = "24";
+
 //store user input
 let assetsAdded = []
 let liabAdded = []
@@ -68,7 +77,6 @@ form.addEventListener('submit', event =>{
             liabDisplay.innerHTML = `${existing} <div><h3>X</h3><h4>${liability.names}</h4 <h4>${liability.number}</h4></div>`
         }
         
-        let agerange = "24";
         let percent = document.getElementById("percent")
         
             if(agerange == "24"){
@@ -258,3 +266,204 @@ form.addEventListener('submit', event =>{
 
 })
 
+const form2 = document.getElementById("agerange");
+form2.addEventListener('submit', event =>{
+    let form2 = document.getElementById("agerange");
+        let agerangeele = form2.elements['age'];
+        agerange = agerangeele.value
+        let percent = document.getElementById("percent")
+        let displayage = document.getElementById("displayAge")
+        
+            if(agerange == "24"){
+                displayage.innerText= "Age 18-24";
+                if(total>=435000){
+                    percent.innerText = "99%"
+                }
+                else if(total>=94000){
+                    percent.innerText = "90-98%"
+                }
+                else if(total >= 25000){
+                    percent.innerText = "75-89%"
+                }
+                else if(total >= 8000){
+                    percent.innerText = "50-74%"
+                }
+                else if(total >= -1400){
+                    percent.innerText = "25-49%"
+                }
+                else{
+                    percent.innerText = "<25%"
+                }
+            }
+            else if(agerange == "29"){
+                displayage.innerText ="Age 25-29";
+                if(total>=606000){
+                    percent.innerText = "99%"
+                }
+                else if(total>=152000){
+                    percent.innerText = "90-98%"
+                }
+                else if(total >= 61000){
+                    percent.innerText = "75-89%"
+                }
+                else if(total >= 7500){
+                    percent.innerText = "50-74%"
+                }
+                else if(total >= -3600){
+                    percent.innerText = "25-49%"
+                }
+                else{
+                    percent.innerText = "<25%"
+                }
+            }
+            else if(agerange == "34"){
+                displayage.innerText = "Age 30-34";
+                if(total>=956000){
+                    percent.innerText ="99%"
+                }
+                else if(total>=258000){
+                    percent.innerText = "90-98%"
+                }
+                else if(total >= 117000){
+                    percent.innerText = "75-89%"
+                }
+                else if(total >= 35000){
+                    percent.innerText = "50-74%"
+                }
+                else if(total >= 2700){
+                    percent.innerText = "25-49%"
+                }
+                else{
+                    percent.innerText = "<25%"
+                }
+            }
+            else if(agerange == "39"){
+                displayage.innerText = "Age 35-39";
+                if(total>=4000000){
+                    percent.innerText = "99%"
+                }
+                else if(total>=601000){
+                    percent.innerText = "90-98%"
+                }
+                else if(total >= 228000){
+                    percent.innerText = "75-89%"
+                }
+                else if(total >= 55000){
+                    percent.innerText = "50-74%"
+                }
+                else if(total >= 5100){
+                    percent.innerText = "25-49%"
+                }
+                else{
+                    percent.innerText = "<25%"
+                }
+            }
+            else if(agerange == "44"){
+                displayage.innerText = "Age 40-44";
+                if(total>=7900000){
+                    percent.innerText = "99%"
+                }
+                else if(total>=848000){
+                    percent.innerText = "90-98%"
+                }
+                else if(total >= 351000){
+                    percent.innerText = "75-89%"
+                }
+                else if(total >= 127000){
+                    percent.innerText = "50-74%"
+                }
+                else if(total >= 18000){
+                    percent.innerText = "25-49%"
+                }
+                else{
+                    percent.innerText = "<25%"
+                }
+            }
+            else if(agerange == "49"){
+                displayage.innerText = "Age 44-49";
+                if(total>=11000000){
+                    percent.innerText = "99%"
+                }
+                else if(total>=1300000){
+                    percent.innerText = "90-98%"
+                }
+                else if(total >= 452000){
+                    percent.innerText = "75-89%"
+                }
+                else if(total >= 164000){
+                    percent.innerText = "50-74%"
+                }
+                else if(total >= 19000){
+                    percent.innerText = "25-49%"
+                }
+                else{
+                    percent.innerText = "<25%"
+                }
+            }
+            else if(agerange == "54"){
+                displayage.innerText = "Age 50-54";
+                if(total>=11000000){
+                    percent.innerText = "99%"
+                }
+                else if(total>=1400000){
+                    percent.innerText = "90-98%"
+                }
+                else if(total >= 493000){
+                    percent.innerText = "75-89%"
+                }
+                else if(total >= 171000){
+                    percent.innerText = "50-74%"
+                }
+                else if(total >= 38900){
+                    percent.innerText = "25-49%"
+                }
+                else{
+                    percent.innerText= "<25%"
+                }
+            }
+            else if(agerange == "59"){
+                displayage.innerText = "Age 55-59";
+                if(total>=11000000){
+                    percent.innerText = "99%"
+                }
+                else if(total>=2500000){
+                    percent.innerText = "90-98%"
+                }
+                else if(total >= 644000){
+                    percent.innerText = "75-89%"
+                }
+                else if(total >= 193000){
+                    percent.innerText = "50-74%"
+                }
+                else if(total >= 38800){
+                    percent.innerText = "25-49%"
+                }
+                else{
+                    percent.innerText = "<25%"
+                }
+            }
+            else{
+                displayage.innerText = "Age 60+";
+                if(total>=11000000){
+                    percent.innerText = "99%"
+                }
+                else if(total>=1900000){
+                    percent.innerText ="90-98%"
+                }
+                else if(total >= 735000){
+                    percent.innerText = "75-89%"
+                }
+                else if(total >= 228000){
+                    percent.innerText = "50-74%"
+                }
+                else if(total >= 48800){
+                    percent.innerText = "25-49%"
+                }
+                else{
+                    percent.innerText = "<25%"
+                }
+            }
+        document.getElementById("overlay2").style.display = "none";
+
+
+})
